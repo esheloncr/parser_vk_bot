d5b7681d0cd1409254bafdd75dcbddf1ec672e0f
esheloncr <igoninalexeynn@gmail.com>
2021-01-07 16:40:41 +0300

    add article video check

--- parser.py
+++ parser.py
@@ -9,12 +9,12 @@ headers = {
 content = {} # записать контент в виде "url статьи":[содержимое статьи]
 # Получаем urls статей
 # сделать функцию для проверки подключения
+url_list = []
 pikabu = r.get(url, headers=headers)
 pikabu_src = pikabu.text
 soup = bs(pikabu_src, "lxml")
 soup.prettify()
 get_h2 = soup.findAll(class_="story__title")
-url_list = []
 for item in get_h2:
     get_url = item.find("a")
     article_url = get_url.get("href")
@@ -24,7 +24,6 @@ article_main = []
 article = r.get(url_list[0], headers=headers)
 article_src = article.text
 soup = bs(article_src, "lxml")
-soup.prettify()
 # Парсим h1 тэг
 article_title = soup.find(class_="story__title-link").contents[0]
 article_main.append(article_title)
@@ -32,12 +31,22 @@ article_main.append(article_title)
 article_text = soup.find(class_="story-block story-block_type_text")
 article_main.append(article_text)
 # Проверяем есть-ли видео в статье, если есть - парсим ссылку, если нету - пропускаем
-#article_video = soup.findAll(class_="story-block story-block_type_video")
-article_video = soup.findAll("data-webm")
-print(article_video)
-"""if article_video == None:
-    print("No videos in Article")
+video_urls =[]
+article_video = soup.findAll(class_="player")
+if article_video == []:
+    print("No video in Article")
 else:
-    pass """
+    for gg in article_video:
+        video_urls.append(gg.get("data-webm"))
+    if video_urls[0] == None:
+        print("No video URL")
+    else:
+        video_url = video_urls[0]
+        article_main.append(video_url)
+
+
+print(article_main)
+
+
 
 
